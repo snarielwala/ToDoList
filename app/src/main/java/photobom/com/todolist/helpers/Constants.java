fa58ccc22f882aa27baea9c9e2d87678e62e01d4
Samarth <samarth.narielwala@gmail.com>
2015-12-16 00:24:32 -0500

    Adding new files

---
+++ app/src/main/java/photobom/com/todolist/helpers/Constants.java
@@ -0,0 +1,17 @@
+package photobom.com.todolist.helpers;
+
+import photobom.com.todolist.activities.EditItemActivity;
+
+/**
+ * Created by snarielwala on 12/14/15.
+ */
+public class Constants {
+
+    public static final int RESULT_CODE_CHANGE=1;
+    public static final int RESULT_CODE_NO_CHANGE=2;
+    public static final String OLD_NAME = "oldName";
+    public static final String NEW_NAME = "newName";
+    public static final String POSITION = "position";
+    public static final String SELECTED_ITEM="selectedItem";
+
+}
---
+++ app/src/main/java/photobom/com/todolist/models/Item.java
@@ -0,0 +1,68 @@
+package photobom.com.todolist.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Delete;
+import com.activeandroid.query.Select;
+import com.activeandroid.query.Update;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Table(name = "Item")
+public class Item extends Model implements Serializable {
+
+    // This is a regular field
+    @Column(name = "name")
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    // Make sure to have a default constructor for every ActiveAndroid model
+    public Item(){
+        super();
+    }
+
+    public Item(String name){
+        super();
+        this.name = name;
+    }
+
+    public static Item addItem(String name){
+        Item item = new Item(name);
+        item.save();
+        return item;
+    }
+
+    public static void updateItem(Long id, String name){
+        new Update(Item.class)
+                .set("name = ?", name)
+                .where("Id = ?", id)
+                .execute();
+    }
+
+    public static List<Item> getAllItems(){
+        return new Select()
+                .from(Item.class)
+                .orderBy("Id ASC")
+                .execute();
+    }
+
+    public void deleteItem(int id){
+        new Delete().from(Item.class).where("Id = ?", id).execute();
+    }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
+
+}
